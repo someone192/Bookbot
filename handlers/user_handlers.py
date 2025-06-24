1ccb015b553f76807eb781ee2c7becc1f42ceb9d
Valeryi <valerylebedintzev@yandex.ru>
2025-06-24 21:32:55 +0300

    in progress

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,1 +1,75 @@
- 
+from copy import deepcopy
+
+from aiogram import F, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import CallbackQuery, Message
+from database.database import user_dict_template, users_db
+from filters.filters import IsDelBookmarkCallbackData, IsDigitCallbackData
+from keyboards.bookmarks_kb import (create_bookmarks_keyboard,
+                                    create_edit_keyboard)
+from keyboards.pagination_kb import create_pagination_keyboard
+from lexicon.lexicon import LEXICON
+from services.file_handling import book
+
+router = Router()
+
+@router.message(CommandStart)
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON[message.text])
+    if message.from_user.id not in users_db:
+        users_db[message.from_user.id] = deepcopy(user_dict_template)
+
+@router.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON[message.text])
+
+@router.message(Command(commands='beginning'))
+async def process_beginning_command(message: Message):
+    users_db[message.from_user.id]['page'] = 1
+    text=book[1]
+    await message.answer(
+        text=text,
+        reply_markup=create_pagination_keyboard(
+            'backward',
+            f'{users_db[message.from_user.id]['page']} / {len(book)}',
+            'forward'
+        )
+    )
+
+@router.message(Command(commands='continue'))
+async def process_continue_command(message: Message):
+    text=book[users_db[message.from_user.id]['page']]
+    await message.answer(
+        text=text,
+        reply_markup=create_pagination_keyboard(
+            'backward',
+            f'{users_db[message.from_user.id]['page']} / {len(book)}',
+            'forward'
+        )
+    )
+
+@router.message(Command(commands='bookmarks'))
+async def process_bookmarks_command(message: Message):
+    if users_db[message.from_user.id]['bookmarks']:
+        await message.answer(
+            text=LEXICON[message.text],
+            reply_markup=create_bookmarks_keyboard(
+                *users_db[message.from_user.id]['bookmarks']
+            )
+        )
+    else:
+        await message.answer(text=LEXICON['no bookmarks'])
+
+@router.callback_query(F.data == 'forward')
+async def process_forward_press(callback: CallbackQuery):
+    if users_db[callback.from_user.id]['page'] < len(book):
+        users_db[callback.from_user.id]['page'] += 1
+        text = book[users_db[callback.from_user.id]['page']]
+        await callback.message.edit_text(
+            text=text,
+            reply_markup=create_pagination_keyboard(
+            'backward',
+            f'{users_db[callback.from_user.id]['page']} / {len(book)}',
+            'forward'
+        )
+        )
